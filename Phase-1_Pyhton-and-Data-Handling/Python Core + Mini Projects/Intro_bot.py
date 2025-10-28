0add035d847d78b42573483f2e16cc933c003558
Vanshika Sharma <vanshika11082004@gmail.com>
2025-10-28 13:16:49 +0530

    add basic Personalized Intro Bot Script

---
+++ Phase-1_Pyhton-and-Data-Handling/Python Core + Mini Projects/Intro_bot.py
@@ -0,0 +1,13 @@
+# Personalized Intro bot
+
+print("Welcome to your Personal Intro Bot!")
+
+name = input("What's your name?")
+age = int(input("How old are you?"))
+hobby = input("What's your favorite hobby?")
+dream = input("What's your biggest dream?")
+
+print("\nGenerating your profile...\n")
+
+print(f"Hey {name}! you are {age} years young and love{hobby}.")
+print(f"Keep working toward your dream of {dream} — the world needs it! ")
