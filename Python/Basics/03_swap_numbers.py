0b3ec3ee7cca48cbb642df1105f2a4bd4ed40a53
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-30 10:05:48 +0530

    Add logic to swap two numbers using temp variable

---
+++ Python/Basics/03_swap_numbers.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter a first num : "))
+num2 = int(input("enter a second num : "))
+temp = 0
+print("num1 before swapping",num1)
+print("num2 before swapping", num2)
+
+temp = num1
+num1 = num2
+num2 =temp
+
+print("num1 after swapping : ", num1)
+print("num2 after swapping : ",num2)
