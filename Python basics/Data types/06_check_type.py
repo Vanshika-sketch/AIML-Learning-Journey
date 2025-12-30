af4efa045ebded13b24795e4fe088f39ab59a827
Vanshika Sharma <vanshika11082004@gmail.com>
2025-12-30 17:13:35 +0530

    Demonstrate Python data types using type()

---
+++ Python basics/Data types/06_check_type.py
@@ -0,0 +1,11 @@
+# Program to check the data type of variables
+
+name = "gololo"
+age = 54
+is_everything_ok = True
+height = 5.8
+
+print(type(name))
+print(type(age))
+print(type(is_everything_ok))
+print(type(height))
