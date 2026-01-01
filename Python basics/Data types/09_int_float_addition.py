fdd329abb58dd3dfb298bbbdc0302f223b2ea0b8
Vanshika Sharma <vanshika11082004@gmail.com>
2026-01-01 16:39:00 +0530

    Add integer and float values and display result

---
+++ Python basics/Data types/09_int_float_addition.py
@@ -0,0 +1,18 @@
+
+# Add an integer and a float and print the result.
+
+num1 = int(input("enter an integer: "))
+num2 = float(input("enter float number: "))
+
+result = num1 + num2
+print(result)
+
+
+
+
+
+# num1 = 45
+# num2 = 45.55
+
+# result = num1 + num2
+# print(result)
