d75383806b3ef284f83c8a86a6e4df1f609e214a
Vanshika Sharma <vanshika11082004@gmail.com>
2026-01-01 16:25:28 +0530

    Add examples of explicit type conversion

---
+++ Python basics/Data types/07_type_conversion.py
@@ -0,0 +1,29 @@
+# program to demonstrate type conversion in python
+
+num_int = 10
+num_float = float(num_int)
+
+price = 99.4
+price_int = int(price)
+
+age_str = "21"
+age_int = int(age_str)
+
+value = 100
+value_str = str(value)
+
+is_active= 1
+is_active_bool = bool(is_active)
+
+print(num_int, type(num_int))
+print(num_float, type(num_float))
+
+print(price, type(price))
+print(price_int, type(price_int))
+
+print(age_str, type(age_str))
+print(age_int, type(age_int))
+
+print(value_str, type(value_str))
+
+print(is_active_bool, type(is_active_bool))
