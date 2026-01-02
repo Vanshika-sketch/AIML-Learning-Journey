6fe7a8b09f23d745cbd61e744881336ba5813c29
Vanshika Sharma <vanshika11082004@gmail.com>
2026-01-02 11:12:33 +0530

    Find the largest of two numbers using conditions

---
+++ Python basics/Conditions(if-else)/13_largest_of_two.py
@@ -0,0 +1,12 @@
+#Find the largest of two numbers. 
+
+
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+
+if a > b:
+    print(" a is largest")
+else : 
+    print("b is largest")
